4816e2cf5656ce459b8c8165ce33fcf4b51a59c3
ItsmeSonu23 <sonumish3180@gmail.com>
2025-03-18 18:28:51 +0530

    kajdbvjkb

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
     outDir: 'dist',
     chunkSizeWarningLimit: 10000,
     rollupOptions: {
-      external: ['@mantine/dates', '@mantine/form', '@mantine/tiptap', '@mantine/core', '@mantine/carousel'],
+      external: ['@mantine/dates', '@mantine/form', '@mantine/tiptap', '@mantine/core', '@mantine/carousel', '@mantine/labs/MantineProvider','@mantine/hooks','@mantine/notifications','@mantine/nprogress','@mantine/labs'],
     },
   },
   optimizeDeps: {
-    include: ['@mantine/core', '@mantine/carousel', '@mantine/dates', '@mantine/form', '@mantine/tiptap'],
+    include: ['@mantine/core', '@mantine/carousel', '@mantine/dates', '@mantine/form', '@mantine/tiptap', '@mantine/hooks','@mantine/notifications','@mantine/nprogress','@mantine/labs','@mantine/labs/MantineProvider'],
   },
 });
