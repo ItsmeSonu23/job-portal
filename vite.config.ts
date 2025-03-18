5d524ed3c800f8becae6f89ba7cb7b69959a0dfc
ItsmeSonu23 <sonumish3180@gmail.com>
2025-03-18 18:36:04 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,5 @@ export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 10000,
-    rollupOptions: {
-      external: ['@mantine/dates', '@mantine/form', '@mantine/tiptap', '@mantine/core', '@mantine/carousel', '@mantine/labs/MantineProvider','@mantine/hooks','@mantine/notifications','@mantine/nprogress','@mantine/labs'],
-    },
-  },
-  optimizeDeps: {
-    include: ['@mantine/core', '@mantine/carousel', '@mantine/dates', '@mantine/form', '@mantine/tiptap', '@mantine/hooks','@mantine/notifications','@mantine/nprogress','@mantine/labs','@mantine/labs/MantineProvider'],
-  },
+    chunkSizeWarningLimit: 10000,}
 });
